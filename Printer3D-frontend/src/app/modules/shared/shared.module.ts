2101eeab6755e17f980706e8096437dc2b478560
Adam Antinoo <adamantinoo.gIt@gmail.com>
2020-05-17 21:53:00 +0200

    -[PROJECT] Configure the project to use no ports and be able to be dockerized or isolated from other projects. Updated project setup documentation.

--- Printer3D-frontend/src/app/modules/shared/shared.module.ts
+++ Printer3D-frontend/src/app/modules/shared/shared.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { V1PagePathPanelComponent } from './panels/v1-page-path-panel/v1-page-path-panel.component';
 import { V1DockComponent } from './panels/v1-dock/v1-dock.component';
 import { V1FeatureRenderComponent } from './renders/v1-feature-render/v1-feature-render.component';
-import { ViewerPanelComponent } from './panels/viewer-panel/viewer-panel.component';
+// import { ViewerPanelComponent } from './panels/viewer-panel/viewer-panel.component';
 
 @NgModule({
     declarations: [
         V1PagePathPanelComponent,
         V1DockComponent,
         V1FeatureRenderComponent,
-        ViewerPanelComponent
+        // ViewerPanelComponent
     ],
     imports: [
         CommonModule
@@ -19,7 +19,7 @@ import { ViewerPanelComponent } from './panels/viewer-panel/viewer-panel.compone
         V1PagePathPanelComponent,
         V1DockComponent,
         V1FeatureRenderComponent,
-        ViewerPanelComponent
+        // ViewerPanelComponent
     ]
 })
 export class SharedModule { }
