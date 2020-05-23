90cb710cfc5075bff927336a62c76ce283b8193b
Adam Antinoo <adamantinoo.git@gmail.com>
2020-05-23 11:13:00 +0200

    -[FRONTEND] Include the new endpoints on the Api simulation.

--- Printer3D-frontend/src/app/platform/platform-constants.ts
+++ Printer3D-frontend/src/app/platform/platform-constants.ts
@@ -4,9 +4,4 @@ export const platformconstants = {
     PARTIAL_PART_KEY: '-PARTIAL-PART-KEY-',
     JWTTOKEN_KEY: '-JWTTOKEN_KEY-',
     JWTTOKEN_EXPIRATION_TIME_KEY: '-JWTTOKEN_EXPIRATION_TIME_KEY-',
-    CREDENTIAL_KEY: '-CREDENTIAL-KEY-',
-    VALID_STATE: 'LU5FT0NPTS5JTkZJTklUWS1ERVZFTE9QTUVOVC1WQUxJRCBTVEFURSBTVFJJTkct',
-    DEFAULT_ICON_PLACEHOLDER: '/assets/media/defaulticonplaceholder.png',
-    DEFAULT_AVATAR_PLACEHOLDER: '/assets/media/defaulticonplaceholder.png',
-    DEFAULT_CORPORATION_AVATAR_PLACEHOLDER: '/assets/media/defaultcorporationavatar.png'
 }
