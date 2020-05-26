f91122a76d7abfef8c96aae0c7f500cb427a5fcd
Adam Antinoo <adamantinoo.git@gmail.com>
2020-05-26 13:29:04 +0200

    -[CI] Setting the backend profile to test. -[CI] Adding installation to the development branch

---
+++ Printer3D-backend/src/test/java/org/dimensinfin/printer3d/backend/RunAcceptanceTests.java
@@ -0,0 +1,16 @@
+package org.dimensinfin.printer3d.backend;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = { "src/test/resources/features" },
+		snippets= CAMELCASE,
+		glue = { "org.dimensinfin.printer3d.backend.steps" },
+		plugin = { "pretty", "json:target/cucumber_report.json" },
+		tags = { "not @skip_scenario", "not @front", "not @duplication", "@P3D01.H" })
+public class RunAcceptanceTests {}
