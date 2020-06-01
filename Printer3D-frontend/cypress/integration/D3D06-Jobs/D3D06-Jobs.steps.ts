268c682d7025e7757612e2d3198ba448bc2a74ff
Adam Antinoo <adamantinoo.git@gmail.com>
2020-06-01 16:57:34 +0200

    -[ACCEPTANCE] Trying to fix the drag & drop with cypress.

--- Printer3D-frontend/cypress/integration/D3D06-Jobs/D3D06-Jobs.steps.ts
+++ Printer3D-frontend/cypress/integration/D3D06-Jobs/D3D06-Jobs.steps.ts
@@ -99,6 +99,63 @@ Then('the button {string} has the next properties', function (buttonName: string
 })
 
 When('the Job is dragged and dropped on the Machine', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    cy.get('app-root').find('production-job-list-page')
+        .find('v1-pending-jobs-panel')
+        .find('div').find('.panel-left')
+        .get('[draggable="true"]').first().should('exist')
+    cy.get('app-root').find('production-job-list-page')
+        .find('v1-machines-panel')
+        .find('v1-machine-render').find('.machine-job')
+        .get('[droppable]').first().should('exist')
+
+    const dragSelector = 'v1-pending-jobs-panel .panel-left'
+    const dropSelector = 'v1-machines-panel .machine-job'
+    // cy.log('Getting the coordinates')
+    // cy.get(dropSelector).should('exist').first().then($drop => 
+    //     coords = $drop[0].getBoundingClientRect()
+    // )
+    // cy.get(dropSelector).should('exist').first().then($el => cy.log('New Coords: '+ JSON.stringify($el[0].getBoundingClientRect())))
+    cy.get(dropSelector).should('exist').first().then($drop => {
+        let coords = $drop[0].getBoundingClientRect()
+        cy.log('Coords: ' + JSON.stringify(coords))
+        cy.get('app-root').find('production-job-list-page')
+            .find('v1-pending-jobs-panel')
+            .find('div').find('.panel-left')
+            .get('[draggable="true"]').first().should('exist')
+            .first().then($drag => {
+                $drag[0].dispatchEvent(<any>new MouseEvent('mousedown'));
+                $drag[0].dispatchEvent(<any>new MouseEvent('mousemove', { clientX: 10, clientY: 0 }));
+                $drag[0].dispatchEvent(<any>new MouseEvent('mousemove', {
+                    clientX: coords.left + 10,
+                    clientY: coords.top + 10
+                }));
+                $drag[0].dispatchEvent(<any>new MouseEvent('mouseup'));
+
+                cy.get('app-root').find('production-job-list-page')
+                    .find('v1-machines-panel')
+                    .find('v1-machine-render').find('.machine-job')
+                    .find('v1-pending-job-render').should('exist')
+            });
+    })
+
+    //     cy.get(target)
+    //         .then($target => {
+    //             let coords = $target[0].getBoundingClientRect();
+    //  })
+
+    // const draggable = Cypress.$(dragSelector)[0]; // Pick up this
+    // const droppable = Cypress.$(dropSelector)[0]; // Drop over this
+
+    // Do the Drg & Drop moving the item
+    // cy.log(JSON.stringify(droppable))
+    // const coords = droppable.getBoundingClientRect();
+    // const coords = { top: 100, left: 400 }
+    // I had to add (as any here --> maybe this can help solve the issue??)
+    // }));
+
+    // Check the job is on place
+    // cy.get('app-root').find('production-job-list-page')
+    //     .find('v1-machines-panel')
+    //     .find('v1-machine-render').find('.machine-job')
+    //     .find('v1-pending-job-render').should('exist')
 });
