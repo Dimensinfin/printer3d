ea6eacd1abc2dafb33e0d770d3c557e82f8f5467
Adam Antinoo <adamantinoo.git@gmail.com>
2020-06-05 17:12:51 +0200

    -[BACKEND][BUG] Fixed the serializer problem. Was a problem related to null job installment dates.

--- Printer3D-backend/src/main/java/org/dimensinfin/printer3d/backend/inventory/machine/serializer/BuildRecordSerializer.java
+++ Printer3D-backend/src/main/java/org/dimensinfin/printer3d/backend/inventory/machine/serializer/BuildRecordSerializer.java
@@ -8,17 +8,24 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import org.springframework.boot.jackson.JsonComponent;
 
 import org.dimensinfin.printer3d.client.inventory.rest.dto.BuildRecord;
+import org.dimensinfin.printer3d.client.inventory.rest.dto.BuildState;
 
 @JsonComponent
-public class BuildRecordSerializer extends JsonSerializer<BuildRecord>{
+public class BuildRecordSerializer extends JsonSerializer<BuildRecord> {
 	@Override
 	public void serialize( final BuildRecord value, final JsonGenerator jgen, final SerializerProvider serializers ) throws IOException {
 		jgen.writeStartObject();
 
-		jgen.writeStringField( "state", value.getState().toString() );
+		if (null != value.getState())
+			jgen.writeStringField( "state", value.getState().toString() );
+		else
+			jgen.writeStringField( "state", BuildState.IDLE.toString() );
 		jgen.writeNumberField( "partCopies", value.getPartCopies() );
 		jgen.writeObjectField( "part", value.getPart() );
-		jgen.writeStringField( "jobInstallmentDate", value.getJobInstallmentDate().toString() );
+		if (null != value.getJobInstallmentDate())
+			jgen.writeStringField( "jobInstallmentDate", value.getJobInstallmentDate().toString() );
+		else
+			jgen.writeStringField( "jobInstallmentDate", null );
 		jgen.writeNumberField( "remainingTime", value.getRemainingTime() );
 
 		jgen.writeEndObject();
