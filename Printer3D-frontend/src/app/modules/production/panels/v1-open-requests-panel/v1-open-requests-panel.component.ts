5998b8c20aff55147e0a50e8892e8a7967c160db
Adam Antinoo <adamantinoo.git@gmail.com>
2020-06-29 01:03:51 +0200

    -[FRONTEND] Completing the creation of Requests. Go to the next step.

--- Printer3D-frontend/src/app/modules/production/panels/v1-open-requests-panel/v1-open-requests-panel.component.ts
+++ Printer3D-frontend/src/app/modules/production/panels/v1-open-requests-panel/v1-open-requests-panel.component.ts
@@ -16,6 +16,7 @@ import { Part } from '@domain/Part.domain';
 import { Request } from '@domain/Request.domain';
 import { IPartProvider } from '@domain/interfaces/IPartProvider.interface';
 import { V1OpenRequestsPageComponent } from '../../pages/v1-open-requests-page/v1-open-requests-page.component';
+import { Model } from '@domain/inventory/Model.domain';
 /**
  * To display some of the Request details we should have access to the list of Parts because the Request List from the backend will not have the Part details but only the Part identifier.
  * The Process should lookup on the Parts list for the Part instance.
@@ -28,6 +29,7 @@ import { V1OpenRequestsPageComponent } from '../../pages/v1-open-requests-page/v
 export class V1OpenRequestsPanelComponent extends AppPanelComponent implements OnInit, Refreshable, IPartProvider {
     @Input() page: V1OpenRequestsPageComponent;
     private parts: Part[] = []
+    private models: Model[] = []
 
     constructor(protected backendService: BackendService) {
         super();
@@ -52,6 +54,8 @@ export class V1OpenRequestsPanelComponent extends AppPanelComponent implements O
 
     // - R E F R E S H A B L E
     public clean(): void {
+        this.parts=[]
+        this.models=[]
     }
     public refresh(): void {
         this.clean()
@@ -74,17 +78,41 @@ export class V1OpenRequestsPanelComponent extends AppPanelComponent implements O
                 }))
                 .subscribe((response: PartListResponse) => {
                     this.parts = response.getParts();
-                    this.downloadRequests();
+                    this.downloadModels();
+                })
+        )
+    }
+    protected downloadModels(): void {
+        this.backendConnections.push(
+            this.backendService.apiInventoryGetModels_v1(new ResponseTransformer()
+                .setDescription('Transforms response into a list of Models.')
+                .setTransformation((entrydata: any): Model[] => {
+                    // For each of the Models expand the Parts from the part provider.
+                    const modelList: Model[] = []
+                    for (const entry of entrydata.models) {
+                        const model: Model = new Model(entry)
+                        for (let index = 0; index < entry.partIdList.length; index++) {
+                            const partFound = this.findById(entry.partIdList[index])
+                            if (undefined != partFound) model.addPart(partFound)
+                        }
+                        modelList.push(model)
+                    }
+                    return modelList
+                }))
+                .subscribe((response: Model[]) => {
+                    this.models = response
+                    this.downloadRequests()
                 })
         )
     }
     protected downloadRequests(): void {
         this.backendConnections.push(
-            this.backendService.apiProductionGetOpenRequests_v1(new ResponseTransformer()
+            this.backendService.apiProductionGetOpenRequests_v2(new ResponseTransformer()
                 .setDescription('Transforms response into a list of Requests.')
                 .setTransformation((entrydata: any): Request[] => {
-                    // Extract requests from the response.
+                    // Extract requests from the response and convert them to the Request V2 format. Resolve contents id references.
                     const requestList: Request[] = []
+                    const requestConstructor : RequestConstructor = new RequestConstructor(this)
                     for (let entry of entrydata.requests) {
                         const request: Request = new Request(entry)
                         request.setPartProvider(this)
