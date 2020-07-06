09051e4be656ac0bfdae4708757ff394e661fcc1
Adam Antinoo <adamantinoo.git@gmail.com>
2020-07-06 15:06:51 +0200

    -[REFACTOR] Removed the V1PagePathPanel that is not being used.

--- Printer3D-frontend/src/app/modules/shared/shared.module.ts
+++ Printer3D-frontend/src/app/modules/shared/shared.module.ts
@@ -2,8 +2,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // - COMPONENTS
-import { V1PagePathPanelComponent } from './panels/v1-page-path-panel/v1-page-path-panel.component';
-import { V1DockComponent } from '../common/v1-dock/v1-dock.component';
+// import { V1PagePathPanelComponent } from './panels/v1-page-path-panel/v1-page-path-panel.component';
+// import { V1DockComponent } from '../common/v1-dock/v1-dock.component';
 // import { V1FeatureRenderComponent } from '../renders/v1-feature-render/v1-feature-render.component';
 import { BackgroundEnabledComponent } from './core/background-enabled/background-enabled.component';
 import { RendersModule } from '../renders/renders.module';
@@ -16,16 +16,16 @@ import { AppPanelComponent } from './core/app-panel/app-panel.component';
         RendersModule
     ],
     declarations: [
-        V1PagePathPanelComponent,
-        V1DockComponent,
+        // V1PagePathPanelComponent,
+        // V1DockComponent,
         // V1FeatureRenderComponent,
         BackgroundEnabledComponent,
         AppPanelComponent,
         ViewerPanelComponent
     ],
     exports: [
-        V1PagePathPanelComponent,
-        V1DockComponent,
+        // V1PagePathPanelComponent,
+        // V1DockComponent,
         // V1FeatureRenderComponent,
         BackgroundEnabledComponent,
         AppPanelComponent,
