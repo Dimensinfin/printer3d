442588833f87a68577e752ae639c3fca63ea67d0
Adam Antinoo <adamantinoo.git@gmail.com>
2020-07-17 20:02:18 +0200

    -[POC] Search for a library to render bar graphs.

--- Printer3D-frontend/src/app/modules/common/common.module.ts
+++ Printer3D-frontend/src/app/modules/common/common.module.ts
@@ -2,28 +2,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgDragDropModule } from 'ng-drag-drop';
+import { NgxChartsModule }from '@swimlane/ngx-charts';
 // - APPLICATION MODULES
 import { RendersModule } from '../renders/renders.module';
 // - COMPONENTS
 import { V1DockComponent } from './v1-dock/v1-dock.component';
 import { V1DropPartPanelComponent } from './v1-drop-part-panel/v1-drop-part-panel.component';
 import { V1WorkLoadPanelComponent } from './v1-work-load-panel/v1-work-load-panel.component';
+import { V1BillingChartPanelComponent } from './v1-billing-chart-panel/v1-billing-chart-panel.component';
 
 @NgModule({
     imports: [
         CommonModule,
         NgDragDropModule.forRoot(),
+        NgxChartsModule,
         RendersModule
     ],
     declarations: [
         V1DockComponent,
         V1DropPartPanelComponent,
-        V1WorkLoadPanelComponent
+        V1WorkLoadPanelComponent,
+        V1BillingChartPanelComponent
     ],
     exports: [
         V1DockComponent,
         V1DropPartPanelComponent,
-        V1WorkLoadPanelComponent
+        V1WorkLoadPanelComponent,
+        V1BillingChartPanelComponent
     ]
 })
 export class AppCommonModule { }
