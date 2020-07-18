d8b7224297cfe680c0d6a5de34a1892c9316ad93
Adam Antinoo <adamantinoo.git@gmail.com>
2020-07-18 08:56:25 +0200

    -[TESTING] Fixing incomplete test.

--- Printer3D-backend/src/test/java/org/dimensinfin/printer3d/backend/inventory/model/rest/v1/ModelServiceV1Test.java
+++ Printer3D-backend/src/test/java/org/dimensinfin/printer3d/backend/inventory/model/rest/v1/ModelServiceV1Test.java
@@ -2,6 +2,7 @@ package org.dimensinfin.printer3d.backend.inventory.model.rest.v1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Assertions;
@@ -9,8 +10,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import org.dimensinfin.core.exception.DimensinfinRuntimeException;
 import org.dimensinfin.printer3d.backend.inventory.model.persistence.ModelEntity;
 import org.dimensinfin.printer3d.backend.inventory.model.persistence.ModelRepository;
+import org.dimensinfin.printer3d.backend.inventory.part.persistence.PartEntity;
 import org.dimensinfin.printer3d.backend.inventory.part.persistence.PartRepository;
 import org.dimensinfin.printer3d.client.inventory.rest.dto.Model;
 import org.dimensinfin.printer3d.client.inventory.rest.dto.ModelList;
@@ -22,6 +25,18 @@ import static org.dimensinfin.printer3d.backend.support.TestDataConstants.ModelC
 import static org.dimensinfin.printer3d.backend.support.TestDataConstants.ModelConstants.TEST_MODEL_LABEL;
 import static org.dimensinfin.printer3d.backend.support.TestDataConstants.ModelConstants.TEST_MODEL_PRICE;
 import static org.dimensinfin.printer3d.backend.support.TestDataConstants.ModelConstants.TEST_MODEL_STOCK_LEVEL;
+import static org.dimensinfin.printer3d.backend.support.TestDataConstants.PartConstants.TEST_PART_BUILD_TIME;
+import static org.dimensinfin.printer3d.backend.support.TestDataConstants.PartConstants.TEST_PART_COLOR;
+import static org.dimensinfin.printer3d.backend.support.TestDataConstants.PartConstants.TEST_PART_COST;
+import static org.dimensinfin.printer3d.backend.support.TestDataConstants.PartConstants.TEST_PART_DESCRIPTION;
+import static org.dimensinfin.printer3d.backend.support.TestDataConstants.PartConstants.TEST_PART_ID;
+import static org.dimensinfin.printer3d.backend.support.TestDataConstants.PartConstants.TEST_PART_IMAGE_PATH;
+import static org.dimensinfin.printer3d.backend.support.TestDataConstants.PartConstants.TEST_PART_LABEL;
+import static org.dimensinfin.printer3d.backend.support.TestDataConstants.PartConstants.TEST_PART_MATERIAL;
+import static org.dimensinfin.printer3d.backend.support.TestDataConstants.PartConstants.TEST_PART_MODEL_PATH;
+import static org.dimensinfin.printer3d.backend.support.TestDataConstants.PartConstants.TEST_PART_PRICE;
+import static org.dimensinfin.printer3d.backend.support.TestDataConstants.PartConstants.TEST_PART_STOCK_AVAILABLE;
+import static org.dimensinfin.printer3d.backend.support.TestDataConstants.PartConstants.TEST_PART_STOCK_LEVEL;
 
 public class ModelServiceV1Test {
 
@@ -41,26 +56,10 @@ public class ModelServiceV1Test {
 	}
 
 	@Test
-	public void newModel() {
-		// Given
-		final ModelRequest modelRequest =		 new ModelRequest.Builder()
-				.withId( TEST_MODEL_ID )
-				.withLabel( TEST_MODEL_LABEL )
-				.withPrice( TEST_MODEL_PRICE )
-				.withStockLevel( TEST_MODEL_STOCK_LEVEL )
-				.withImagePath( TEST_MODEL_IMAGE_PATH )
-				.withPartIdList( new ArrayList<>() )
-				.build();
-		// Test
-		final ModelServiceV1 modelServiceV1 = new ModelServiceV1( this.modelRepository, this.partRepository );
-		final Model obtained = modelServiceV1.newModel( modelRequest );
-	}
-
-	@Test
 	public void getModels() {
 		final List<ModelEntity> modelEntityList = new ArrayList<>();
 		modelEntityList.add( new ModelEntity.Builder()
-				.withId( UUID.fromString("9ed337f1-eb5a-48ff-969f-6991e0f75918") )
+				.withId( UUID.fromString( "9ed337f1-eb5a-48ff-969f-6991e0f75918" ) )
 				.withLabel( TEST_MODEL_LABEL )
 				.withPrice( TEST_MODEL_PRICE )
 				.withStockLevel( TEST_MODEL_STOCK_LEVEL )
@@ -69,7 +68,7 @@ public class ModelServiceV1Test {
 				.build()
 		);
 		modelEntityList.add( new ModelEntity.Builder()
-				.withId( UUID.fromString("b783c038-04cd-4290-a7c5-9820f9e38991") )
+				.withId( UUID.fromString( "b783c038-04cd-4290-a7c5-9820f9e38991" ) )
 				.withLabel( TEST_MODEL_LABEL )
 				.withPrice( TEST_MODEL_PRICE )
 				.withStockLevel( TEST_MODEL_STOCK_LEVEL )
@@ -86,5 +85,102 @@ public class ModelServiceV1Test {
 		Assertions.assertNotNull( obtained );
 		Assertions.assertEquals( 2, obtained.getCount() );
 		Assertions.assertEquals( 2, obtained.getModels().size() );
+	}
+
+	@Test
+	public void newModel() {
+		// Given
+		final ModelRequest modelRequest = new ModelRequest.Builder()
+				.withId( TEST_MODEL_ID )
+				.withLabel( TEST_MODEL_LABEL )
+				.withPrice( TEST_MODEL_PRICE )
+				.withStockLevel( TEST_MODEL_STOCK_LEVEL )
+				.withImagePath( TEST_MODEL_IMAGE_PATH )
+				.withPartIdList( new ArrayList<>() )
+				.build();
+		final List<UUID> partIdList = new ArrayList<>();
+		partIdList.add( TEST_PART_ID );
+		final ModelEntity modelEntity = new ModelEntity.Builder()
+				.withId( TEST_MODEL_ID )
+				.withLabel( TEST_MODEL_LABEL )
+				.withPrice( TEST_MODEL_PRICE )
+				.withStockLevel( TEST_MODEL_STOCK_LEVEL )
+				.withImagePath( TEST_MODEL_IMAGE_PATH )
+				.withActive( TEST_MODEL_ACTIVE )
+				.withPartIdList( partIdList )
+				.build();
+		final PartEntity partEntity = new PartEntity.Builder()
+				.withId( TEST_PART_ID )
+				.withLabel( TEST_PART_LABEL )
+				.withDescription( TEST_PART_DESCRIPTION )
+				.withMaterial( TEST_PART_MATERIAL )
+				.withColor( TEST_PART_COLOR )
+				.withBuildTime( TEST_PART_BUILD_TIME )
+				.withCost( TEST_PART_COST )
+				.withPrice( TEST_PART_PRICE )
+				.withStockLevel( TEST_PART_STOCK_LEVEL )
+				.withStockAvailable( TEST_PART_STOCK_AVAILABLE )
+				.withImagePath( TEST_PART_IMAGE_PATH )
+				.withModelPath( TEST_PART_MODEL_PATH )
+				.withActive( false )
+				.build();
+		// When
+		Mockito.when( this.modelRepository.findById( Mockito.any( UUID.class ) ) ).thenReturn( Optional.empty() );
+		Mockito.when( this.modelRepository.save( Mockito.any( ModelEntity.class ) ) ).thenReturn( modelEntity );
+		Mockito.when( this.partRepository.findById( Mockito.any( UUID.class ) ) ).thenReturn( Optional.of( partEntity ) );
+		// Test
+		final ModelServiceV1 modelServiceV1 = new ModelServiceV1( this.modelRepository, this.partRepository );
+		final Model obtained = modelServiceV1.newModel( modelRequest );
+		// Assertions
+		Assertions.assertNotNull( obtained );
+		Assertions.assertEquals( TEST_PART_ID.toString(), obtained.getPartList().get( 0 ).getId().toString() );
+	}
+
+	@Test
+	public void newModelException() {
+		// Given
+		final ModelRequest modelRequest = new ModelRequest.Builder()
+				.withId( TEST_MODEL_ID )
+				.withLabel( TEST_MODEL_LABEL )
+				.withPrice( TEST_MODEL_PRICE )
+				.withStockLevel( TEST_MODEL_STOCK_LEVEL )
+				.withImagePath( TEST_MODEL_IMAGE_PATH )
+				.withPartIdList( new ArrayList<>() )
+				.build();
+		final List<UUID> partIdList = new ArrayList<>();
+		partIdList.add( TEST_PART_ID );
+		final ModelEntity modelEntity = new ModelEntity.Builder()
+				.withId( TEST_MODEL_ID )
+				.withLabel( TEST_MODEL_LABEL )
+				.withPrice( TEST_MODEL_PRICE )
+				.withStockLevel( TEST_MODEL_STOCK_LEVEL )
+				.withImagePath( TEST_MODEL_IMAGE_PATH )
+				.withActive( TEST_MODEL_ACTIVE )
+				.withPartIdList( partIdList )
+				.build();
+		final PartEntity partEntity = new PartEntity.Builder()
+				.withId( TEST_PART_ID )
+				.withLabel( TEST_PART_LABEL )
+				.withDescription( TEST_PART_DESCRIPTION )
+				.withMaterial( TEST_PART_MATERIAL )
+				.withColor( TEST_PART_COLOR )
+				.withBuildTime( TEST_PART_BUILD_TIME )
+				.withCost( TEST_PART_COST )
+				.withPrice( TEST_PART_PRICE )
+				.withStockLevel( TEST_PART_STOCK_LEVEL )
+				.withStockAvailable( TEST_PART_STOCK_AVAILABLE )
+				.withImagePath( TEST_PART_IMAGE_PATH )
+				.withModelPath( TEST_PART_MODEL_PATH )
+				.withActive( false )
+				.build();
+		// When
+		Mockito.when( this.modelRepository.findById( Mockito.any( UUID.class ) ) ).thenReturn( Optional.of( modelEntity ) );
+		Mockito.when( this.modelRepository.save( Mockito.any( ModelEntity.class ) ) ).thenReturn( modelEntity );
+		Mockito.when( this.partRepository.findById( Mockito.any( UUID.class ) ) ).thenReturn( Optional.of( partEntity ) );
+		// Test
+		final ModelServiceV1 modelServiceV1 = new ModelServiceV1( this.modelRepository, this.partRepository );
+		Assertions.assertThrows( DimensinfinRuntimeException.class, () -> {
+			modelServiceV1.newModel( modelRequest );
+		} );
 	}
 }
