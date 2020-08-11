26047d31bc9b89d8fee3acf7fba6cec5e0c4d621
Adam Antinoo <adamantinoo.git@gmail.com>
2020-08-11 18:21:30 +0200

    -[REFACTOR] Moving domain classes to their respective information unit package.

--- Printer3D-frontend/src/app/AppErrorHandler.ts
+++ Printer3D-frontend/src/app/AppErrorHandler.ts
@@ -1,28 +1,8 @@
 // - CORE
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-import { OnDestroy } from '@angular/core';
-import { Input } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { ErrorHandler } from '@angular/core';
 import { Injector } from '@angular/core';
-// - SERVICES
-import { BackendService } from '@app/services/backend.service';
 // - DOMAIN
-import { PartRecord } from '@domain/PartRecord.domain';
-import { GridColumn } from '@domain/GridColumn.domain';
-import { ResponseTransformer } from '@app/services/support/ResponseTransformer';
-import { PartListResponse } from '@domain/dto/PartListResponse.dto';
-import { Part } from '@domain/Part.domain';
-import { EVariant } from '@domain/interfaces/EPack.enumerated';
-import { AppPanelComponent } from '@app/modules/shared/core/app-panel/app-panel.component';
-import { PartContainer } from '@domain/PartContainer.domain';
-import { environment } from '@env/environment';
-import { Refreshable } from '@domain/interfaces/Refreshable.interface';
-import { BackgroundEnabledComponent } from './modules/shared/core/background-enabled/background-enabled.component';
-import { BackendInfoResponse } from '@domain/dto/BackendInfoResponse.dto';
-import { IsolationService } from './platform/isolation.service';
-import { HttpErrorResponse } from '@angular/common/http';
 import { RollbarService } from 'angular-rollbar';
 
 @Injectable()
--- Printer3D-frontend/src/app/app.component.ts
+++ Printer3D-frontend/src/app/app.component.ts
@@ -6,14 +6,7 @@ import { Input } from '@angular/core';
 // - SERVICES
 import { BackendService } from '@app/services/backend.service';
 // - DOMAIN
-import { PartRecord } from '@domain/PartRecord.domain';
-import { GridColumn } from '@domain/GridColumn.domain';
 import { ResponseTransformer } from '@app/services/support/ResponseTransformer';
-import { PartListResponse } from '@domain/dto/PartListResponse.dto';
-import { Part } from '@domain/Part.domain';
-import { EVariant } from '@domain/interfaces/EPack.enumerated';
-import { AppPanelComponent } from '@app/modules/shared/core/app-panel/app-panel.component';
-import { PartContainer } from '@domain/PartContainer.domain';
 import { environment } from '@env/environment';
 import { Refreshable } from '@domain/interfaces/Refreshable.interface';
 import { BackgroundEnabledComponent } from './modules/shared/core/background-enabled/background-enabled.component';
