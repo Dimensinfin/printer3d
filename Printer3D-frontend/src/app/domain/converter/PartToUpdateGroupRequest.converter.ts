1d77e5768c53cbd538aee79f77a66e87e0e42d9c
Adam Antinoo <adamantinoo.git@gmail.com>
2021-04-22 12:21:43 +0200

    -[FRONTEND] Adding the Project field to the Part Container editor.

--- Printer3D-frontend/src/app/domain/converter/PartToUpdateGroupRequest.converter.ts
+++ Printer3D-frontend/src/app/domain/converter/PartToUpdateGroupRequest.converter.ts
@@ -2,11 +2,16 @@
 import { Part } from '@domain/inventory/Part.domain';
 import { UpdateGroupRequest } from '@domain/dto/UpdateGroupRequest.dto';
 import { Converter } from '@domain/interfaces/Converter.interface';
+import { Printer3DConstants } from '@app/platform/Printer3DConstants.platform';
 
 export class PartToUpdateGroupRequestConverter implements Converter<Part, UpdateGroupRequest> {
     public convert(input: Part): UpdateGroupRequest {
+        let projectName: string
+        if (input.project) projectName = input.project
+        else projectName = Printer3DConstants.DEFAULT_PROJECT_NAME
         return new UpdateGroupRequest({
             label: input.label,
+            project: projectName,
             description: input.description,
             weight: input.weight,
             buildTime: input.buildTime,
