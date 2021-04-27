f717b86059c8f9ac59aada9587875908bdfe6aa6
Adam Antinoo <adamantinoo.git@gmail.com>
2021-04-27 20:34:18 +0200

    -[SECURITY] Refactor security configuration to only allow access to the backend from the selected server.

--- Printer3D-backend/src/main/java/org/dimensinfin/printer3d/backend/config/security/SecurityPermitAllConfig.java
+++ Printer3D-backend/src/main/java/org/dimensinfin/printer3d/backend/config/security/SecurityPermitAllConfig.java
@@ -2,6 +2,7 @@ package org.dimensinfin.printer3d.backend.config.security;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,10 +13,13 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+	@Value("${spring.security.originDomains}")
+	private String allowedOrigins;
+
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		final CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins( Arrays.asList( "http://localhost:5102" ) );
+		configuration.setAllowedOrigins( Arrays.asList( this.allowedOrigins, "https://printer3d-frontend.herokuapp.com/" ) );
 		configuration.setAllowedMethods( Arrays.asList( "OPTIONS", "GET", "POST", "PATCH" ) );
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration( "/**", configuration );
@@ -31,7 +35,7 @@ public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
 	protected void configure( final HttpSecurity http ) throws Exception {
 		http.authorizeRequests()
 				.anyRequest().permitAll()
-				//				.and().cors()
+				.and().cors()
 				.and().csrf().disable();
 	}
 }
