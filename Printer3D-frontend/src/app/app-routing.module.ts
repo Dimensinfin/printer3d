274529180214c4575a8df5ebc5940b1ef73157dd
Adam Antinoo <adamantinoo.git@gmail.com>
2021-07-16 21:21:41 +0200

    -[FRONTEND] Chenges to the machine rendering to pot the machines on an horizontal list.

--- Printer3D-frontend/src/app/app-routing.module.ts
+++ Printer3D-frontend/src/app/app-routing.module.ts
@@ -5,18 +5,18 @@ import { Routes } from '@angular/router';
 import { RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/',
-    pathMatch: 'full'
-  },
-  { path: 'inventory', loadChildren: () => import('./modules/inventory/inventory.module').then(m => m.InventoryModule) },
-  { path: 'production', loadChildren: () => import('./modules/production/production.module').then(m => m.ProductionModule) },
-  { path: 'extractions', loadChildren: () => import('./modules/extraction/extraction.module').then(m => m.ExtractionModule) }
+    {
+        path: '',
+        redirectTo: '/',
+        pathMatch: 'full'
+    },
+    { path: 'inventory', loadChildren: () => import('./modules/inventory/inventory.module').then(m => m.InventoryModule) },
+    { path: 'production', loadChildren: () => import('./modules/production/production.module').then(m => m.ProductionModule) },
+    { path: 'extractions', loadChildren: () => import('./modules/extraction/extraction.module').then(m => m.ExtractionModule) }
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
