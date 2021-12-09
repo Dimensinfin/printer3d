9595d1d553bcaf6fecbe5c7b619e4dd494cd6758
Adam Antinoo <adamantinoo.git@gmail.com>
2021-12-09 14:38:55 +0100

    -[TESTING] Removing unimplemented tests.

--- Printer3D-frontend/src/app/modules/production/pages/v2-production-job-list-page/v2-production-job-list-page.component.spec.ts
+++ Printer3D-frontend/src/app/modules/production/pages/v2-production-job-list-page/v2-production-job-list-page.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { V2ProductionJobListPageComponent } from './v2-production-job-list-page.component';
 
-describe('V2ProductionJobListPageComponent', () => {
+xdescribe('V2ProductionJobListPageComponent', () => {
   let component: V2ProductionJobListPageComponent;
   let fixture: ComponentFixture<V2ProductionJobListPageComponent>;
 
