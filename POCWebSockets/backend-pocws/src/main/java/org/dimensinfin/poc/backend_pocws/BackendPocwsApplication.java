30db3cd1498b7df89c9fff6fb6ad4120b08dbf12
adam antinoo <adamantinoo.git@gmail.com>
2024-11-30 12:14:40 +0100

    -[POC] Upgrade POCWebSockets to new application model.

---
+++ POCWebSockets/backend-pocws/src/main/java/org/dimensinfin/poc/backend_pocws/BackendPocwsApplication.java
@@ -0,0 +1,13 @@
+package org.dimensinfin.poc.backend_pocws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPocwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPocwsApplication.class, args);
+	}
+
+}
---
+++ POCWebSockets/backend-pocws/src/test/java/org/dimensinfin/poc/backend_pocws/BackendPocwsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dimensinfin.poc.backend_pocws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPocwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
