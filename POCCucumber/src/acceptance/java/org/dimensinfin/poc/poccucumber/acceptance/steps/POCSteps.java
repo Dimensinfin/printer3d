e6fc108860b7a569b9259d58a28129a901e6cd1e
adam antinoo <adamantinoo.git@gmail.com>
2024-12-04 15:54:21 +0100

    -[POC] Add a second scenery for Coil creation.
    -[TASK] Add new cucumber scenarios to check that command line test can run them.

--- POCCucumber/src/acceptance/java/org/dimensinfin/poc/poccucumber/acceptance/steps/POCSteps.java
+++ POCCucumber/src/acceptance/java/org/dimensinfin/poc/poccucumber/acceptance/steps/POCSteps.java
@@ -5,18 +5,27 @@ import javax.validation.constraints.NotNull;
 
 import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import org.dimensinfin.poc.poccucumber.acceptance.support.POCWorld;
+import org.dimensinfin.poc.poccucumber.application.usecases.CoilServiceV2;
+import org.dimensinfin.poc.poccucumber.domain.Coil;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class POCSteps extends StepSupport {
+	private final CoilServiceV2 coilService;
+
 	@Autowired
-	public POCSteps( @NotNull final POCWorld world ) {
+	public POCSteps( @NotNull final POCWorld world, final CoilServiceV2 coilService ) {
 		super( world );
+		this.coilService = coilService;
 	}
 
 	@Given("a clean Coils repository")
@@ -27,7 +36,6 @@ public class POCSteps extends StepSupport {
 
 	@When("the Get Coils v2 request is processed")
 	public void the_get_coils_v2_request_is_processed() {
-		// Write code here that turns the phrase above into concrete actions
 		this.getWorld().setHttpStatus( HttpStatus.OK );
 	}
 
@@ -40,4 +48,20 @@ public class POCSteps extends StepSupport {
 	public void the_list_of_coils_has_items( final Integer itemCount ) {
 		Assertions.assertEquals( itemCount.intValue(), this.getWorld().getCoils().size() );
 	}
+
+	@When("the Create Coils v2 request is processed")
+	public void the_create_coils_v2_request_is_processed() {
+		final String url = "http://localhost:9001/api/v2/inventory/coils"; // Adjust the URL as needed
+		final HttpHeaders headers = new HttpHeaders();
+		headers.set( "Content-Type", "application/json" );
+
+		final HttpEntity<Coil> requestEntity = new HttpEntity<>( Coil.builder()
+				.name( "New Coil" )
+				.build(), headers );
+		final ResponseEntity<Coil> responseEntity = new RestTemplate().postForEntity( url, requestEntity, Coil.class );
+		this.getWorld().setResponseEntityCoil( responseEntity );
+		this.getWorld().setHttpStatus( HttpStatus.CREATED );
+		this.getWorld().setCoils( this.coilService.getCoils() );
+	}
+
 }
--- POCCucumber/src/main/java/org/dimensinfin/poc/poccucumber/infrastructure/adapters/inbound/rest/CoilControllerV2.java
+++ POCCucumber/src/main/java/org/dimensinfin/poc/poccucumber/infrastructure/adapters/inbound/rest/CoilControllerV2.java
@@ -1,18 +1,20 @@
 package org.dimensinfin.poc.poccucumber.infrastructure.adapters.inbound.rest;
 
 import java.util.List;
-
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.dimensinfin.poc.poccucumber.domain.Coil;
 import org.dimensinfin.poc.poccucumber.application.usecases.CoilServiceV2;
+import org.dimensinfin.poc.poccucumber.domain.Coil;
 
 /**
  * @author Adam Antinoo (adamantinoo.git@gmail.com)
@@ -35,5 +37,14 @@ public class CoilControllerV2 {
 			produces = "application/json")
 	public ResponseEntity<List<Coil>> getCoils() {
 		return new ResponseEntity<>( this.coilServiceV2.getCoils(), HttpStatus.OK );
+	}
+
+	@PostMapping(
+			value = "/inventory/coils",
+			produces = { "application/json" },
+			consumes = { "application/json" }
+	)
+	public ResponseEntity<Coil> createPart( @Valid @RequestBody final Coil coilDto ) {
+		return ResponseEntity.ok( ConverterFactory.toCoil( this.coilServiceV2.createCoil( coilDto ) ) );
 	}
 }
